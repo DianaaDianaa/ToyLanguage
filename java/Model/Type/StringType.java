6ae688b66bd035a3f813a212a35c396c23910dec
DianaaDianaa <125254001+DianaaDianaa@users.noreply.github.com>
2023-02-13 12:44:53 +0200

    Add files via upload

---
+++ java/Model/Type/StringType.java
@@ -0,0 +1,22 @@
+package Model.Type;
+
+
+import Model.Value.StringValue;
+import Model.Value.Value;
+
+public class StringType implements Type {
+    @Override
+    public boolean equals(Type anotherType) {
+        return anotherType instanceof StringType;
+    }
+
+    @Override
+    public Value defaultValue() {
+        return new StringValue("");
+    }
+
+    @Override
+    public String toString() {
+        return "string";
+    }
+}
---
+++ java/Model/Type/Type.java
@@ -0,0 +1,8 @@
+package Model.Type;
+
+import Model.Value.Value;
+
+public interface Type {
+    boolean equals(Type anotherType);
+    Value defaultValue();
+}
